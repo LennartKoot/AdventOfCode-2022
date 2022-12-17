c83781276483a8714202250b024c92825b3dc3cc
Koot van, Lennart <lennartvkoot@gmail.com>
2022-12-18 00:42:50 +0100

    feat: Day 05, Part Two

--- fsharp/05/Program.fs
+++ fsharp/05/Program.fs
@@ -3,11 +3,14 @@ open SupplyStacks.Instructions
 open SupplyStacks.Stacks
 
 type State = { Stacks: Stacks; Instructions: Instruction list }
+type Crane =
+    | CraneMover9000 // Part One
+    | CraneMover9001 // Part Two
 
 let exampleInput = loadInput "data/example.txt"
 let realInput = loadInput "data/input.txt"
 
-let applyInstruction (stacks: Stacks) (instruction: Instruction): Stacks =
+let applyInstruction (stacks: Stacks) (instruction: Instruction) (crane: Crane): Stacks =
     let ((movingCrates: Crate list), (sourceStack: Stack)) =
         stacks[instruction.Source - 1] |> List.splitAt (instruction.Amount)
     
@@ -17,14 +20,16 @@ let applyInstruction (stacks: Stacks) (instruction: Instruction): Stacks =
         | n when (n = instruction.Source - 1) -> 
             sourceStack
         | n when (n = instruction.Destination - 1) ->
-            (movingCrates |> List.rev)@stack
+            match crane with
+            | CraneMover9000 -> (movingCrates |> List.rev)@stack
+            | CraneMover9001 -> movingCrates@stack
         | _ ->
             stack)
 
-let rec executeInstructions (state: State) : State =
+let rec executeInstructions (state: State) (crane: Crane) : State =
     match state.Instructions with
     | [] -> state
-    | x::xs -> executeInstructions { Stacks = applyInstruction state.Stacks x; Instructions = xs}
+    | x::xs -> executeInstructions { Stacks = applyInstruction state.Stacks x crane; Instructions = xs} crane
 
 let startState input = 
     let (stacks, remainingInput) = parseStacks input
@@ -42,6 +47,11 @@ let realStartState = startState realInput
 let realEndState = executeInstructions realStartState
 
 printfn "Part one"
-printfn "Example Data: %s" (determineTopCrates exampleEndState.Stacks |> printCrates)
-printfn "Real Data: %A" (determineTopCrates realEndState.Stacks |> printCrates)
+printfn "Example Data: %s" (determineTopCrates (exampleEndState CraneMover9000).Stacks |> printCrates)
+printfn "Real Data: %A" (determineTopCrates (realEndState CraneMover9000).Stacks |> printCrates)
+printfn ""
+
+printfn "Part two"
+printfn "Example Data: %s" (determineTopCrates (exampleEndState CraneMover9001).Stacks |> printCrates)
+printfn "Real Data: %A" (determineTopCrates (realEndState CraneMover9001).Stacks |> printCrates)
 printfn ""
