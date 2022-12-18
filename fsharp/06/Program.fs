220484251794cca8b9060c4d651804272d53f7c9
Koot van, Lennart <lennartvkoot@gmail.com>
2022-12-18 22:35:47 +0100

    feat: Day 06, Part One

---
+++ fsharp/06/Program.fs
@@ -0,0 +1,28 @@
+﻿open System.IO
+
+// Solution
+
+let readInput (relativePath: string): string =
+    let filename: string = Path.Combine(__SOURCE_DIRECTORY__, relativePath)
+    File.ReadAllText(filename)
+
+let isMarker s =
+    s |> Set.ofSeq |> Set.count = 4
+
+let findMarkerIndex input =
+    input
+    |> Seq.windowed 4
+    |> Seq.findIndex isMarker
+    |> (+) 4
+
+// Running and output
+
+let readExampleInput n = readInput $"data/example{n}.txt"
+
+let example = readExampleInput 1 |> findMarkerIndex
+let real = readInput "data/input.txt" |> findMarkerIndex
+
+printfn "Part one"
+printfn "Example Data: %A" example
+printfn "Real Data: %A" real
+printfn ""
