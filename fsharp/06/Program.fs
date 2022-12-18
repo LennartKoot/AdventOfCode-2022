83f3feb10b0e13502c858a372ab2f15aff3086fb
Koot van, Lennart <lennartvkoot@gmail.com>
2022-12-18 22:41:58 +0100

    feat: Day 06, Part Two

--- fsharp/06/Program.fs
+++ fsharp/06/Program.fs
@@ -6,23 +6,34 @@ let readInput (relativePath: string): string =
     let filename: string = Path.Combine(__SOURCE_DIRECTORY__, relativePath)
     File.ReadAllText(filename)
 
-let isMarker s =
-    s |> Set.ofSeq |> Set.count = 4
+let isMarker n s =
+    s |> Set.ofSeq |> Set.count = n
 
-let findMarkerIndex input =
+let findMarkerIndex n input =
     input
-    |> Seq.windowed 4
-    |> Seq.findIndex isMarker
-    |> (+) 4
+    |> Seq.windowed n
+    |> Seq.findIndex (isMarker n)
+    |> (+) n
+
+let findStartOfPacketMarker = findMarkerIndex 4
+let findStartOfMessageMarker = findMarkerIndex 14
 
 // Running and output
 
 let readExampleInput n = readInput $"data/example{n}.txt"
 
-let example = readExampleInput 1 |> findMarkerIndex
-let real = readInput "data/input.txt" |> findMarkerIndex
+let example1 = readExampleInput 1 |> findStartOfPacketMarker
+let real1 = readInput "data/input.txt" |> findStartOfPacketMarker
 
 printfn "Part one"
-printfn "Example Data: %A" example
-printfn "Real Data: %A" real
+printfn "Example Data: %A" example1
+printfn "Real Data: %A" real1
+printfn ""
+
+let example2 = readExampleInput 1 |> findStartOfMessageMarker
+let real2 = readInput "data/input.txt" |> findStartOfMessageMarker
+
+printfn "Part two"
+printfn "Example Data: %A" example2
+printfn "Real Data: %A" real2
 printfn ""
