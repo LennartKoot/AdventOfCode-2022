0bd4e83ffd03f90cdeeb83e2cd52d93c4f8e0dfd
Koot van, Lennart <lennartvkoot@gmail.com>
2022-12-24 16:38:16 +0100

    feat: Determine sum of directory sizes

--- fsharp/07/Program.fs
+++ fsharp/07/Program.fs
@@ -16,4 +16,40 @@ let determineDirectorySizes =
         Directory (dir, subTrees |> List.sumBy sumBy, subTrees)
     foldUpToRoots fFile fDirectory
 
-printfn "FileSystem with directory sizes: %A" <| (filesystem "data/example.txt" |> determineDirectorySizes)
+let flatten fFile fDirectory (trees: ItemTreeWithDirectorySize list) =
+    let rec flattenTree acc tree =
+        match tree with
+        | File file -> 
+            fFile file 
+            |> function 
+                | Some f -> f :: acc
+                | None -> acc
+        | Directory (dir,size,subTress) ->
+            let subFlattened = subTress |> List.map (flattenTree []) |> List.collect id
+            fDirectory (dir, size)
+            |> function
+                | Some d -> d :: subFlattened
+                | None -> subFlattened
+    trees |> List.map (flattenTree []) |> List.collect id
+
+let flattenAndFilterDirectories maxSize =
+    let fFile f = None
+    let fDirectory (d,size) =
+        match size with
+        | s when size < maxSize -> Some s
+        | _ -> None
+    flatten fFile fDirectory
+
+printfn "Part one (example): %A" <| ( 
+    filesystem "data/example.txt" 
+    |> determineDirectorySizes
+    |> flattenAndFilterDirectories 100000
+    |> List.sum
+)
+
+// printfn "Part one (real): %A" <| ( 
+//     filesystem "data/input.txt" 
+//     |> determineDirectorySizes
+//     |> flattenAndFilterDirectories 100000
+//     |> List.sum
+// )
